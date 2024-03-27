b0a5b5c58ad8a43b9cfcf30b4b03a3a285a59491
manishapariyar <manishapariyar196@gmail.com>
2024-03-27 09:36:55 +0545

    cart section is completed

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -18,10 +18,16 @@ const StoreContextProvider = (props) => {
     setCartItems((prev) =>({...prev,[itemId]:prev[itemId]-1}))
   }
 
-
-   useEffect(()=>{
-    console.log(cartItems)
-   },[cartItems])
+  const getCartTotal = () =>{
+    let totalAmount = 0;
+    for(const item in cartItems){
+      if(cartItems[item]>0){
+        let itemInfo = food_list.find((product)=>product._id === item);
+        totalAmount += itemInfo.price* cartItems[item];
+      } 
+    }
+    return totalAmount;
+  }
 
 
 
@@ -32,7 +38,8 @@ const StoreContextProvider = (props) => {
     cartItems, 
     setCartItems,
     addToCart,
-    removeFromCart
+    removeFromCart,
+    getCartTotal
   } 
 
   return (
